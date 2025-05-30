54f7ec5dce05ab07756663ab540af57374f61ffc
jamespham <jpham07@student.ubc.ca>
2025-05-29 20:01:25 -0600

    Initial commit

---
+++ tumor-classifier.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+
+datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=0.2
+)
+
+training_set = datagen.flow_from_directory(
+    'dataset',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='binary',
+    subset='training'
+)
+
+test_set = datagen.flow_from_directory(
+    'dataset',
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='binary',
+    subset='validation'
+)
+
+cnn = tf.keras.models.Sequential()
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+cnn.add(tf.keras.layers.Flatten())
+cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+history = cnn.fit(training_set, validation_data = test_set, epochs = 25)
+
+cnn.save("tumor_classifier_model.h5")
+
+plt.plot(history.history['accuracy'], label='Train Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Model Accuracy Over Time')
+plt.legend()
+plt.grid(True)
+plt.show()
